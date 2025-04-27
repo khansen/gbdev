3ef300e9ef0e01c514fa1f5e5c8073c3b6000044
Kent Hansen <kentmhan@gmail.com>
2025-04-27 14:30:15 +0200

    Double Dragon II remix (J-SMS)

---
+++ music/double-dragon-ii-sms/song.s
@@ -0,0 +1,154 @@
+; Square channels
+song_chn0_instrument_table:
+song_chn1_instrument_table:
+song_chn2_instrument_table:
+.dw env0
+.db $00,$00,$00,$00,$00,$00 ; 0
+.dw env1
+.db $00,$02,$18,$00,$00,$00 ; 1 snare
+.dw env9
+.db $00,$02,$F0,$00,$00,$00 ; 2 bass drum
+
+; Noise channel
+song_chn3_instrument_table:
+.dw env3
+.db $00,$00,$00,$00,$00,$00 ; 0 noise (closed)
+.dw env4
+.db $00,$00,$00,$00,$00,$00 ; 1 noise (open)
+.dw env5
+.db $00,$00,$00,$00,$00,$00 ; 2 noise (snare)
+.dw env7
+.db $00,$00,$00,$00,$00,$00 ; 3 weird noise (closed) - reduce
+.dw env0
+.db $00,$00,$00,$00,$00,$00 ; 4 noise (infinite)
+
+; Melodic FM channels
+song_chn4_instrument_table:
+song_chn5_instrument_table:
+song_chn6_instrument_table:
+song_chn7_instrument_table:
+song_chn8_instrument_table:
+song_chn9_instrument_table:
+.dw env13
+.db $00,$00,$00,$70,$00,$00 ; 0 short bass
+.dw env0
+.db $00,$00,$00,$70,$00,$00 ; 1 infinite bass
+.dw env15
+.db $00,$00,$00,$70,$00,$00 ; 2 very short bass
+.dw env0
+.db $00,$00,$00,$60,$00,$00 ; 3 lead - $A0
+.dw env0
+.db $00,$00,$00,$f0,$00,$00 ; 4
+.dw env14
+.db $00,$00,$00,$e0,$00,$00 ; 5 another bass
+.dw env0
+.db $00,$00,$00,$90,$00,$00 ; 6
+
+env12:
+.db $20
+.db $04,$80,$FF
+.db $04,$00,$00
+.db $FF,$FF
+
+env13:
+.db $F0
+.db $00,$F0,$06
+.db $20,$00,$00
+.db $FF,$FF
+
+env14:
+.db $F0
+.db $08,$00,$00
+.db $FF,$FF
+
+env15:
+.db $F0
+.db $00,$F0,$02
+.db $30,$00,$00
+.db $FF,$FF
+
+env16:
+.db $E0
+.db $00,$E0,$FF
+.db $04,$00,$00
+.db $FF,$FF
+
+; Rhythm FM channels
+song_chn10_instrument_table:
+song_chn11_instrument_table:
+song_chn12_instrument_table:
+.dw env10
+.db $00,$00,$00,$10,$00,$00 ; 0 - bass drum
+.dw env10
+.db $00,$00,$00,$01,$00,$00 ; 1 - closed hi hat
+.dw env10
+.db $00,$00,$00,$08,$00,$00 ; 2 - snare
+.dw env10
+.db $00,$00,$00,$02,$00,$00 ; 3 - open hi hat/cymbal
+.dw env10
+.db $00,$00,$00,$04,$00,$00 ; 4 - tomtom
+
+env0:
+.db $F0
+.db $00,$F0,$FF
+.db $20,$00,$00
+.db $FF,$FF
+env3:
+.db $F0
+.db $38,$00,$00
+.db $FF,$FF
+env4:
+.db $F0
+.db $00,$F0,$02
+.db $24,$00,$00
+.db $FF,$FF
+env5:
+.db $F0
+.db $00,$F0,$01
+.db $1E,$00,$00
+.db $FF,$FF
+env6:
+.db $F0
+.db $00,$F0,$01
+.db $20,$10,$00
+.db $70,$80,$00
+.db $10,$10,$00
+.db $FF,$FF
+env1:
+.db $F0
+.db $18,$00,$00
+.db $FF,$FF
+env2:
+.db $F0
+.db $00,$F0,$02
+.db $40,$10,$00
+.db $90,$A0,$01
+.db $28,$10,$00
+.db $50,$60,$01
+.db $10,$10,$00
+.db $FF,$FF
+env7:
+.db $30
+.db $04,$00,$00
+.db $FF,$FF
+env8:
+.db $E0
+.db $07,$00,$00
+.db $FF,$FF
+env9:
+.db $F0
+.db $00,$F0,$02
+.db $30,$00,$00
+.db $FF,$FF
+env10:
+.db $F0
+.db $00,$F0,$09
+.db $80,$00,$00
+.db $FF,$FF
+env11:
+.db $F0
+.db $00,$F0,$04
+.db $80,$00,$00
+.db $FF,$FF
+
+.include "song.inc"
